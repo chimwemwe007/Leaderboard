f5ac800f999c1a92502c297b37c6a2f97d8a844d
Chimwemwe Mkandawire <chimwe.wire@gmail.com>
2022-07-21 21:14:36 +0200

    Configure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,37 @@
-const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Template',
-      template: './src/index.html',
-    }),
-  ],
+
   devServer: {
     static: './dist',
   },
+
   module: {
     rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
     ],
   },
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+
+  devtool: 'source-map',
+
+  optimization: {
+    runtimeChunk: 'single',
   },
 };
