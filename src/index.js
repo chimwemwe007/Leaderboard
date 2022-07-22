68796f234bef74304fdd8d501822fa0b420b35f9
Chimwemwe Mkandawire <chimwe.wire@gmail.com>
2022-07-22 20:35:41 +0200

    Add functionality to buttons refresh and submit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-import './style.css';
+import './index.css';
+import loadScores from './refresh-scores.js';
+import { submitScore } from './add-score.js';
+
+loadScores();
+
+const refresh = document.querySelector('.refresh');
+refresh.addEventListener('click', () => {
+  loadScores();
+});
+
+const submit = document.querySelector('.submit');
+submit.addEventListener('click', () => {
+  const user = document.querySelector('#user').value;
+  const score = document.querySelector('#score').value;
+  submitScore(user, score);
+});
